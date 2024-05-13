2de42b314d9938ea47cd3dfaec7c50eb869bb6a4
Miguel Martínez <leugimmanre@gmail.com>
2024-05-13 23:37:00 +0200

    First commit

---
+++ src/components/order/OrderSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function OrderSidebar() {
+  return (
+    <aside className="md:w-72 md:h-screen bg-white">
+      <nav className="mt-10">
+
+      </nav>
+    </aside>
+  )
+}
